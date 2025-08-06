1114d260ae22a7c6e48d24eff8496ce43d0d3e9e
Carbon2301 <trinhhuuan372@gmail.com>
2025-08-06 14:46:35 +0700

    Organization

---
+++ bkflow/config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: 'BKFlow',
+  description: 'BKFlow 是一个基于 Node.js 的流程引擎，提供了强大的流程建模和执行功能，支持多种流程类型和复杂的业务逻辑。',
+};
---
+++ bkflow/middleware.ts
@@ -0,0 +1,37 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
+
+const isSelectOrgRoute = createRouteMatcher(["/select-org(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, orgId } = await auth();
+
+  if (!userId && !isPublicRoute(req)) {
+    await auth.protect();
+    return;
+  }
+
+  if (userId && isPublicRoute(req)) {
+    const path = orgId ? `/organization/${orgId}` : "/select-org";
+    return NextResponse.redirect(new URL(path, req.url));
+  }
+
+  if (userId && !orgId && !isSelectOrgRoute(req)) {
+    return NextResponse.redirect(new URL("/select-org", req.url));
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
