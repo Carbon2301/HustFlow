45bed7f8255dababe8f0f6262af3d4142fc8f114
Carbon2301 <trinhhuuan372@gmail.com>
2025-08-16 23:19:35 +0700

    create useAction abstraction

--- bkflow/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ bkflow/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,21 +1,34 @@
 "use client";
 
-import { create } from "@/actions/create-board";
 import { Button } from "@/components/ui/button";
-import { useActionState } from "react";
+import { createBoard } from "@/actions/create-board/index";
 import { FormInput } from "./form-input";
+import { useAction } from "@/hooks/use-action";
 
 export const Form = () => {
-    const initialState = { message: null, errors: {} };
-    const [state, dispatch, isPending] = useActionState(create, initialState);
+    const { execute, fieldErrors } = useAction(createBoard, {
+        onSuccess: (data) => {
+            console.log("Board created:", data);
+        },
+        onError: (error) => {
+            console.error("Error creating board:", error);
+        }
+    });
+
+    const onSubmit = (formData: FormData) => {
+        const title = formData.get("title") as string;
+
+        execute({ title });
+    }
+
     return (
-        <form action={dispatch}>
+        <form action={onSubmit}>
             <div className="flex flex-col space-y-2">
-                <FormInput errors={state?.errors} />
+                <FormInput errors={fieldErrors} />
             </div>
-          <Button type="submit" disabled={isPending}>
-            Submit
-          </Button>
+            <Button type="submit" className="mt-4">
+                Submit
+            </Button>
         </form>
-    )
-}
+    );
+};
